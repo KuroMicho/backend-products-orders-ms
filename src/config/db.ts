9e0be7f39cc8481f097f142b4e5bcdd047c53225
KuroMicho <kevin0552009.kr@gmail.com>
2025-04-02 15:28:07 -0500

    [deploy] changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,16 +3,25 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-	process.env.NODE_ENV === "production" ? process.env.DATABASE_URL : process.env.DATABASE_TEST_URL,
-	{
-		dialect: "postgres",
-		dialectOptions: {
-			ssl: false,
-			connectTimeout: 5000,
-		},
-		logging: console.log,
-	}
-);
+// Verificación de variables de entorno
+const databaseUrl =
+	process.env.NODE_ENV === "production" ? process.env.DATABASE_URL : process.env.DATABASE_TEST_URL;
+
+if (!databaseUrl) {
+	throw new Error("Database URL is not defined in environment variables");
+}
+
+const sequelize = new Sequelize(databaseUrl, {
+	dialect: "postgres",
+	dialectOptions: {
+		ssl:
+			process.env.NODE_ENV === "production"
+				? {
+						rejectUnauthorized: false,
+				  }
+				: false,
+	},
+	logging: console.log,
+});
 
 export { sequelize };
